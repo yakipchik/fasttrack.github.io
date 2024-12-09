c77151b26128460e9ee6608f006f6fd5b3422f3b
Артём <yakipchik@gmail.com>
2024-12-09 18:23:55 +0300

    change

--- assets/js/sendForm.js
+++ assets/js/sendForm.js
@@ -1,29 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-	function sendForm(event, php) {
-		event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-		var req = new XMLHttpRequest();
-		req.open("POST", php, true);
-		req.onload = function () {
-			if (req.status >= 200 && req.status < 400) {
-				json = JSON.parse(this.response);
-				if (json.result == "success") {
-					event.reset();
-					// document.querySelector(".success-popup").classList.add("open");
-					event.querySelector("button").disabled = false;
-				} else {
-					alert("Ошибка. Сообщение не отправлено");
-					event.querySelector("button").disabled = false;
-				}
-			} else {
-				alert("Ошибка сервера. Повторите отправку");
-				event.querySelector("button").disabled = false;
-			}
-		};
-		req.onerror = function () {
-			alert("Ошибка отправки запроса");
-			event.querySelector("button").disabled = false;
-		};
-		req.send(new FormData(event));
+	const url = window.location.href + "/form/send.php";
+
+	// async function sendFrm() {
+	// 	let response = await fetch(url);
+	// 	if (response.ok) {
+	// 		let json = await response.json();
+	// 	} else {
+	// 		alert("Ошибка HTTP: " + response.status);
+	// 	}
+	// }
+
+	async function sendForm(event, php) {
+		let response = await fetch(url);
+		if (response.ok) {
+			let json = await response.json();
+		} else {
+			alert("Ошибка HTTP: " + response.status);
+		}
+		console.log(response);
+
+		// event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+		// var req = new XMLHttpRequest();
+		// req.open("POST", php, true);
+		// req.onload = function () {
+		// 	if (req.status >= 200 && req.status < 400) {
+		// 		json = JSON.parse(this.response);
+		// 		if (json.result == "success") {
+		// 			event.reset();
+		// 			// document.querySelector(".success-popup").classList.add("open");
+		// 			event.querySelector("button").disabled = false;
+		// 		} else {
+		// 			alert("Ошибка. Сообщение не отправлено");
+		// 			event.querySelector("button").disabled = false;
+		// 		}
+		// 	} else {
+		// 		console.log(json);
+		// 		alert("Ошибка сервера. Повторите отправку");
+		// 		event.querySelector("button").disabled = false;
+		// 	}
+		// };
+		// req.onerror = function () {
+		// 	alert("Ошибка отправки запроса");
+		// 	event.querySelector("button").disabled = false;
+		// };
+		// req.send(new FormData(event));
 	}
 
 	let forms = document.querySelectorAll("form");
